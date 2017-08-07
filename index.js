d22dd80b9eb34b10f3ef484f923af5767f866031
wk645 <wk645@nyu.edu>
2017-08-07 15:26:20 -0400

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten){
+    kittens.push(kitten);
+}
+
+function destructivelyPrependKitten(kitten){
+    kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastKitten(kitten){
+    kittens.pop(kitten);
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+    kittens.shift(kitten);
+}
+function appendKitten(kitten){
+    let kittensCopy = kittens.slice(0);
+    kittensCopy.push(kitten);
+    return kittensCopy;
+}
+
+function prependKitten(kitten){
+    let kittensCopy = kittens.slice(0);
+    kittensCopy.unshift(kitten);
+    return kittensCopy;
+}
+
+function removeLastKitten(kitten){
+    let kittensCopy = kittens.slice(0);
+    kittensCopy.pop(kitten);
+    return kittensCopy;
+}
+
+function removeFirstKitten(kitten){
+    let kittensCopy = kittens.slice(0);
+    kittensCopy.shift(kitten);
+    return kittensCopy;
+}
